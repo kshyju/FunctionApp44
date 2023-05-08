6b0d917955cc996d66d37d4f49bd751524f7ca42
Shyju Krishnankutty <connectshyju@gmail.com>
2023-05-08 15:14:13 -0700

    Updated isolated SDK packages to latest stable.

--- FunctionApp44/Function1.cs
+++ FunctionApp44/Function1.cs
@@ -22,7 +22,7 @@ namespace FunctionApp44
             var response = req.CreateResponse(HttpStatusCode.OK);
             response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
 
-            response.WriteString("Welcome to Azure Functions44!");
+            response.WriteString("Welcome to Azure Funct10ns44!");
 
             return response;
         }
