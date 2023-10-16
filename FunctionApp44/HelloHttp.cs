3195857904a455c1ee77a571728beba9392092dc
Shyju Krishnankutty <connectshyju@gmail.com>
2023-10-16 09:05:45 -0700

    net6 ga ready worker packages

--- FunctionApp44/HelloHttp.cs
+++ FunctionApp44/HelloHttp.cs
@@ -5,16 +5,16 @@ using Microsoft.Extensions.Logging;
 
 namespace FunctionApp44
 {
-    public class Function1
+    public class HelloHttp
     {
         private readonly ILogger _logger;
 
-        public Function1(ILoggerFactory loggerFactory)
+        public HelloHttp(ILoggerFactory loggerFactory)
         {
-            _logger = loggerFactory.CreateLogger<Function1>();
+            _logger = loggerFactory.CreateLogger<HelloHttp>();
         }
 
-        [Function("Function1")]
+        [Function("hellohttp")]
         public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
--- FunctionApp44/Program.cs
+++ FunctionApp44/Program.cs
@@ -1,9 +1,6 @@
 using Microsoft.Extensions.Hosting;
-using Microsoft.Azure.Functions.Worker;
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
-    .ConfigureGeneratedFunctionMetadataProvider()
-    .ConfigureGeneratedFunctionExecutor()
     .Build();
 
 host.Run();
